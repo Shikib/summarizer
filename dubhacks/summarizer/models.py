2c550b9efbdd5a0ef840237e287ac01387164fa8
Shikib Mehri <mehrishikib@gmail.com>
2015-10-18 12:45:41 -0700

    fixed searching to use cached value

--- dubhacks/summarizer/models.py
+++ dubhacks/summarizer/models.py
@@ -4,21 +4,21 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class UserProfile(models.Model):
-	user = models.OneToOneField(User)
-	def __str__(self):
-		return self.user.username
+    user = models.OneToOneField(User)
+    def __str__(self):
+        return self.user.username
 
 class Topic(models.Model):
-	title = models.CharField(max_length=200)
-	subscribers = models.ManyToManyField(UserProfile)
-	def __str__(self):
-		return self.title
+    title = models.CharField(max_length=200)
+    subscribers = models.ManyToManyField(UserProfile)
+    updated = models.DateField()
+    def __str__(self):
+        return self.title
 
 class Summary(models.Model):
-	title = models.TextField(blank=True)
-	text = models.TextField()
-	url = models.URLField(blank=True)
-	date = models.DateTimeField('date updated')
-	topics = models.ManyToManyField(Topic)
-	def __str__(self):
-		return self.text
+    title = models.TextField(blank=True)
+    text = models.TextField()
+    url = models.URLField(blank=True)
+    topics = models.ManyToManyField(Topic)
+    def __str__(self):
+        return self.text
