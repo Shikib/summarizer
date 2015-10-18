4e4894e200e0a3001a878e19bc1fc739b08456e8
Shikib Mehri <mehrishikib@gmail.com>
2015-10-17 21:57:55 -0700

    finished sbfs/dbfs

--- algo/sim.py
+++ algo/sim.py
@@ -1,12 +1,50 @@
 import math
 
 pos_scores = [0.17, 0.23, 0.14, 0.08, 0.05, 0.04, 0.06, 0.04, 0.04, 0.15, 0]
+epsilon = 1e-10
+
 
 def title_score(sentence, title):
     return csim(words(sentence), (title))
 
-def keyword_score(sentence, keyword):
-        
+def keyword_score(sentence, keywords):
+    return sbfs(sentence, keywords) * dbfs(sentence, keywords)    
+
+
+def sbfs(sentence, keywords):
+    summ = 0.0
+    for w in sentence.split():
+        if w in keyword.keys():
+             summ += keyword[w]
+    
+    return summ/len(sentence)
+    
+# keyword scores normalized between 0 and 1
+def dbfs(sentence, keywords):
+    keyword_count = 0
+    summ = 0.0
+    last_score = 0.0
+    last_index = 0
+    words = sentence.split():
+    for i in range(0, len(words)):
+        w = words[i]
+        if w in keyword.keys():
+            keyword_count += 1
+            if not last_index:
+                summ += (last_score*keywords[w])/pow(i - last_index)
+            last_score = keywords[w]
+            last_index = i
+
+    if not keyword_count:
+      return 0
+    
+    if keyword_count == 1:
+      return epsilon
+
+    return summ/(keyword_count - 1) 
+    
+    
+    
 
 def length_score(sentence)
     return lenth(sentence)
